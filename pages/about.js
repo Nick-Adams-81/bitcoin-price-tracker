664ce5a9d4ba6a3a46e320926571585b3197d63a
nick adams <thebigdeal916@icloud.com>
2020-08-08 14:25:23 -0700

    title update

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 const About = () => (
     <Layout>
         <div>
-            <h1>About BitzPrice</h1>
+            <h1>About bitcoin price tracker</h1>
             <p>App to view bitcoin prices</p>
 
         </div>
